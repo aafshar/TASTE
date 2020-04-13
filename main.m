fb270b86ca888790e96f8700c474eed2bed12bd2
Xingchi Li <38049831+nathanxli@users.noreply.github.com>
2020-04-12 21:20:31 -0400

    added preprocessing for new data

--- main.m
+++ main.m
@@ -5,29 +5,35 @@ close all;
 addpath(genpath('./TASTE_Framework'));
 addpath(genpath('./nonnegfac-matlab-master')); % this package is from https://www.cc.gatech.edu/~hpark/nmfsoftware.php
 
-A = readtable('static_feature.csv');
-XK = readtable('design_matrix_2.csv');
-[K, P] = size(A);
-X_height = height(XK);
-R = max(XK{:, 3});
-X = cell(K, 1);
-j = 1;
-for k = 1:K
-    start = j;
-    while (j <= X_height) && strcmp(A{k, 1}{1}, XK{j, 1}{1})
-        j = j + 1;
+if isfile("prep.mat")
+    load("prep.mat");
+else
+    A = readtable('static_feature.csv');
+    XK = readtable('design_matrix_3.csv');
+    [K, P] = size(A);
+    X_height = height(XK);
+    R = max(XK{:, 3});
+    X = cell(K, 1);
+    j = 1;
+    for k = 1:K
+        start = j;
+        while (j <= X_height) && strcmp(A{k, 1}{1}, XK{j, 1}{1})
+            j = j + 1;
+        end
+        X{k} = XK{start : (j-1), 2:(end-1)};
     end
-    X{k} = XK{start : (j-1), 2:(end-1)};
+    A = A(:, 2:(end-1));
+    for i = 1:K
+        A{i, 2} = 2010-(A{i, 2} - mod(A{i, 2}, 10000))/10000;
+    end
+    A = A{:, :};
+    for k = 1:K
+        hei = size(X{k});
+        X{k} = sparse(X{k}(:, 1), X{k}(:, 2), ones(hei(1), 1), X{k}(end, 1), R);
+    end
+    save("prep.mat")
 end
-A = A(:, 2:(end-1));
-for i = 1:K
-    A{i, 2} = 2010-(A{i, 2} - mod(A{i, 2}, 10000))/10000;
-end
-A = A{:, :};
-for k = 1:K
-    hei = size(X{k});
-    X{k} = sparse(X{k}(:, 1), X{k}(:, 2), ones(hei(1), 1), X{k}(end, 1), R);
-end
+
 R = 20;
 %%create a synthetic data set.
 % K=100;
